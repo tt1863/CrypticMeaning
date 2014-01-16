8c3ad9d20cb24b5827c2ca12005b6928b8bb2819
tt1863 <tt1863@gmail.com>
2014-01-16 00:33:47 -0500

    Updated Heroku settings

--- CrypticMeaning/wsgi.py
+++ CrypticMeaning/wsgi.py
@@ -12,3 +12,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CrypticMeaning.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+
+#ADD FOR HEROKU
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
