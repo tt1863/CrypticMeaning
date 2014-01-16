77a4bb9375705f01f9dbd4376fb221ed66989623
tt1863 <tt1863@gmail.com>
2014-01-16 13:08:34 -0500

    Added access keys

--- CrypticMeaning/settings.py
+++ CrypticMeaning/settings.py
@@ -145,6 +145,8 @@ INSTALLED_APPS += ('storages',)
 #Causing App crash in Heroku
 if not DEBUG:
     AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+    AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+    AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
     STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
     S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
     STATIC_URL = S3_URL
